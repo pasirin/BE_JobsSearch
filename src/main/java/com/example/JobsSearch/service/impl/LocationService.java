33d8f2528d2d263ef7f717aa05d7ba0cd4e455eb
pasirin <63360413+pasirin@users.noreply.github.com>
2023-08-02 11:24:14 +0700

    Update LocationService.java
    Update location Services to fix the missing file bug

--- src/main/java/com/example/JobsSearch/service/impl/LocationService.java
+++ src/main/java/com/example/JobsSearch/service/impl/LocationService.java
@@ -3,25 +3,35 @@ package com.example.JobsSearch.service.impl;
 import com.example.JobsSearch.model.util.Location;
 import com.example.JobsSearch.repository.util.LocationRepository;
 import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.Scanner;
 
 @Service
 public class LocationService {
+  private static final Logger logger = LoggerFactory.getLogger(LocationService.class);
   @Autowired LocationRepository locationRepository;
 
   @EventListener
   @SneakyThrows
   public void initLocation(ApplicationReadyEvent event) {
-    File file = new File("./src/main/resources/Location.txt");
-    Scanner scanner = new Scanner(file);
+    ClassLoader classLoader = getClass().getClassLoader();
+    InputStream in = classLoader.getResourceAsStream("src/main/resources/Location.txt");
+    if (in == null) {
+      logger.error("Could not find location text file at: src/main/resources/Location.txt");
+      return;
+    }
+    Scanner scanner = new Scanner(new InputStreamReader(in));
     while (scanner.hasNextLine()) {
       String temp = scanner.nextLine();
       if (!locationRepository.existsByCity(temp)) {
